18365489cd715e4757af8ef74c845187edb93c44
Taylor Jadin <jadin.taylor@gmail.com>
2022-03-22 21:21:14 +0000

    just psot types no taxonomies

--- wp-content/plugins/domains-api-ui/domains-api-ui.php
+++ wp-content/plugins/domains-api-ui/domains-api-ui.php
@@ -2,7 +2,7 @@
 /**
 * Plugin Name: Domains API UI
 * Plugin URI: https://github.com/TaylorJadin/domains-api-ui
-* Description: This plugin adds custom post types and taxonomies for the Domains API.
+* Description: This plugin adds custom post types for the Domains API.
 * Version: 0.1
 * Author: Reclaim Hosting
 * Author URI: https://reclaimhosting.com
@@ -14,12 +14,11 @@ add_action('init', function() {
     register_post_type('domains', [
         'label' => __('Domains', 'txtdomain'),
         'public' => true,
-        'menu_position' => 0,
+        'menu_position' => 1.0,
         'menu_icon' => 'dashicons-admin-site',
         'supports' => ['title', 'author'],
         'show_in_rest' => true,
         'rewrite' => ['slug' => 'domain'],
-        'taxonomies' => ['registrar'],
         'labels' => [
             'singular_name' => __('Domain', 'txtdomain'),
             'add_new_item' => __('Add new domain', 'txtdomain'),
@@ -31,32 +30,12 @@ add_action('init', function() {
             'insert_into_item' => __('Insert into domains', 'txtdomain')
         ],		
     ]);
-    
-    register_taxonomy('registrar', ['domains'], [
-        'label' => __('Registrar', 'txtdomain'),
-        'hierarchical' => true,
-        'rewrite' => ['slug' => 'domains-registrar'],
-        'show_admin_column' => true,
-        'show_in_rest' => true,
-        'labels' => [
-            'singular_name' => __('Registrar', 'txtdomain'),
-            'all_items' => __('All Registrars', 'txtdomain'),
-            'edit_item' => __('Edit Registrar', 'txtdomain'),
-            'view_item' => __('View Registrar', 'txtdomain'),
-            'update_item' => __('Update Registrar', 'txtdomain'),
-            'add_new_item' => __('Add New Registrar', 'txtdomain'),
-            'new_item_name' => __('New Registrar Name', 'txtdomain'),
-            'search_items' => __('Search Registrars', 'txtdomain'),
-            'not_found' => __('No Registrars found', 'txtdomain'),
-        ]
-    ]);
-    register_taxonomy_for_object_type('registrar', 'domains');
 
     // Services
     register_post_type('services', [
         'label' => __('Services', 'txtdomain'),
         'public' => true,
-        'menu_position' => 0,
+        'menu_position' => 1.1,
         'menu_icon' => 'dashicons-products',
         'supports' => ['title', 'author'],
         'show_in_rest' => true,
@@ -70,6 +49,27 @@ add_action('init', function() {
             'not_found_in_trash' => __('No services found in trash', 'txtdomain'),
             'all_items' => __('All services', 'txtdomain'),
             'insert_into_item' => __('Insert into services', 'txtdomain')
+        ],		
+    ]);
+
+    // Services
+    register_post_type('applications', [
+        'label' => __('Applications', 'txtdomain'),
+        'public' => true,
+        'menu_position' => 1.2,
+        'menu_icon' => 'dashicons-superhero-alt',
+        'supports' => ['title', 'author'],
+        'show_in_rest' => true,
+        'rewrite' => ['slug' => 'application'],
+        'labels' => [
+            'singular_name' => __('Application', 'txtdomain'),
+            'add_new_item' => __('Add new application', 'txtdomain'),
+            'new_item' => __('New application', 'txtdomain'),
+            'view_item' => __('View application', 'txtdomain'),
+            'not_found' => __('No applications found', 'txtdomain'),
+            'not_found_in_trash' => __('No applications found in trash', 'txtdomain'),
+            'all_items' => __('All applications', 'txtdomain'),
+            'insert_into_item' => __('Insert into applications', 'txtdomain')
         ],		
     ]);
 
