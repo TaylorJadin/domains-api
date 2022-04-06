74cbf39a1a7e41579bb4eef6d49126364f6c6af3
Taylor Jadin <jadin.taylor@gmail.com>
2022-04-06 15:58:00 +0000

    comments

--- wp-content/plugins/domains-api/domains-api.php
+++ wp-content/plugins/domains-api/domains-api.php
@@ -12,7 +12,12 @@
 add_action( 'gform_after_submission_1', 'dooo_create_user', 10, 2 );
 
 function dooo_create_user ( $entry, $form ) {
+    // Get form data
     var_dump($entry);
     $firstname = $entry["4.3"];
+
+    // Call the API 
+    // https://bionicteaching.com/gravity-forms-open-street-map-api-and-leaflet-js/
+    // https://github.com/TaylorJadin/whmcs-scripts/blob/main/getClientGroups.php
 }
 ?>
