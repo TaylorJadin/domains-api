a369c14c53353b7aad5f94f95ed185aeb07cb8c8
Taylor Jadin <jadin.taylor@gmail.com>
2022-04-28 15:40:15 +0000

    don't need gravity forms code for now

--- wp-content/plugins/domains-api/domains-api.php
+++ wp-content/plugins/domains-api/domains-api.php
@@ -8,18 +8,5 @@
 * Author URI: https://reclaimhosting.com
 **/
 
-
-add_action( 'gform_after_submission_1', 'dooo_create_user', 10, 2 );
-
-function dooo_create_user ( $entry, $form ) {
-    // Get form data
-    // https://docs.gravityforms.com/gform_post_submission/
-    
-    var_dump($entry);
-    $firstname = $entry["4.3"];
-
-    // Call the API 
-    // https://bionicteaching.com/gravity-forms-open-street-map-api-and-leaflet-js/
-    // https://github.com/TaylorJadin/whmcs-scripts/blob/main/getClientGroups.php
 }
 ?>
